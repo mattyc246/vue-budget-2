ad3ded41b9a1fbee11e4530d3cf40c6ed69dcbc3
Matthew Cross <mattyc246@gmail.com>
2021-02-05 10:54:16 +0800

    added firestore and app initialization through firestore auth

--- src/main.js
+++ src/main.js
@@ -3,12 +3,20 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import { auth } from "./utils/firebase"
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+let app;
+
+auth.onAuthStateChanged(() => {
+  if(!app){
+    app = new Vue({
+      router,
+      store,
+      vuetify,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
+
