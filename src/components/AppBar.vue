8f9cff75180ea799d6902b0b3e9481843ce95d75
Matthew Cross <mattyc246@gmail.com>
2021-02-05 22:48:26 +0800

    added theme switcher toggle

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <v-app-bar class="px-3" app color="primary" dark>
+    <v-app-bar class="px-3" app color="primary">
       <v-btn icon @click="toggleDrawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
       <v-spacer />
       <v-btn icon @click="logout">
         <v-icon>mdi-logout</v-icon>
+      </v-btn>
+      <v-btn icon @click="toggleDarkMode">
+        <v-icon v-if="$vuetify.theme.dark">mdi-lightbulb-on</v-icon>
+        <v-icon v-else>mdi-lightbulb-off</v-icon>
       </v-btn>
     </v-app-bar>
     <SideDrawer :drawer.sync="drawer" />
@@ -24,16 +28,19 @@ export default {
   props: {
     drawer: {
       type: Boolean,
-      required: true
-    }
+      required: true,
+    },
   },
   methods: {
-    toggleDrawer(){
-      this.$emit('update:drawer', !this.drawer)
+    toggleDrawer() {
+      this.$emit("update:drawer", !this.drawer);
     },
-    logout(){
-      this.$store.dispatch('User/logout')
-    }
-  }
+    toggleDarkMode() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+    logout() {
+      this.$store.dispatch("User/logout");
+    },
+  },
 };
 </script>
