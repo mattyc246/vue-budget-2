320f9a342b35da71b698ab871c317b7312c9d987
Matthew Cross <mattyc246@gmail.com>
2021-02-06 22:46:30 +0800

    use env variables

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3m6XN7a18ipOMbmWEb_6VlPSBu8NHswo",
-  authDomain: "budgetvue.firebaseapp.com",
-  projectId: "budgetvue",
-  storageBucket: "budgetvue.appspot.com",
-  messagingSenderId: "645188678733",
-  appId: "1:645188678733:web:02be352f58204e94365804",
-  measurementId: "G-X5T8Q2H9DC",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
