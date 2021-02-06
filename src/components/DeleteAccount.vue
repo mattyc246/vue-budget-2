60c9add88c3f8cf9c96f29fbac9bb13c97d679bc
Matthew Cross <mattyc246@gmail.com>
2021-02-06 21:37:43 +0800

    added dialog to delete account

---
+++ src/components/DeleteAccount.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent max-width="290">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          class="mx-3"
+          color="error"
+          elevation="5"
+          v-bind="attrs"
+          v-on="on"
+          icon
+          large
+          ><v-icon>mdi-trash-can</v-icon></v-btn
+        >
+      </template>
+      <v-card>
+        <v-card-title class="headline">
+          Are you sure you want to delete this account?
+        </v-card-title>
+        <v-card-text
+          >Deleting the account will delete all the account and all transactions
+          associated with it. This is an irreversible action.</v-card-text
+        >
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="green darken-1" text @click="dialog = false">
+            Cancel
+          </v-btn>
+          <v-btn color="error" text @click="dialog = false"> Delete </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "DeleteAccount",
+  props: {
+    accountId: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
