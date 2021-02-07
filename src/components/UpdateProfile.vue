e8064727dfb819a36eb8ece6640d78be0c625d79
Matthew Cross <mattyc246@gmail.com>
2021-02-07 16:33:32 +0800

    user profile shows user information

---
+++ src/components/UpdateProfile.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Update Profile</h1>
+</template>
+
+<script>
+export default {
+  name: "UpdateProfile",
+};
+</script>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,9 +1,35 @@
 <template>
-  <h1>User Settings</h1>
+  <div class="settings__container">
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="6">
+          <UserProfile />
+        </v-col>
+        <v-col cols="12" md="6">
+          <UpdateProfile />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
+import UserProfile from "../components/UserProfile"
+import UpdateProfile from "../components/UpdateProfile"
+
 export default {
-  name: "Settings"
-}
+  name: "Settings",
+  components: {
+    UserProfile,
+    UpdateProfile
+  }
+};
 </script>
+
+<style lang="scss" scoped>
+.settings__container {
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+}
+</style>
